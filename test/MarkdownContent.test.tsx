7a108664b2dc32ca079d9d28768401ce3ebe6784
clydedz <clyd94@gmail.com>
2021-03-13 19:12:09 +1300

    Convert test to tsx and remove babel

---
+++ test/MarkdownContent.test.tsx
@@ -0,0 +1,38 @@
+import MarkdownContent from "../lib/MarkdownContent";
+import React from "react";
+import axios from "axios";
+import {render, act, screen, cleanup} from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+afterEach(cleanup);
+
+describe("", () => {
+    it("should take a snapshot", async () => {
+        const res = {data: `## About`};
+        mockedAxios.get.mockResolvedValue(res);
+
+        let renderedComponent;
+        await act(async () => {
+            renderedComponent = render(<MarkdownContent content="" />); 
+        });
+
+        expect(renderedComponent).toMatchSnapshot()
+    });
+
+    it("should return correct HTML", async () => {
+        const res = {data: "## About \r _bold text_"};
+        mockedAxios.get.mockResolvedValue(res);
+
+        await act(async () => {
+            render(<MarkdownContent content="" />);
+        });
+
+        const linkElement = await screen.findByTestId("mdcontent");
+        expect(linkElement.innerHTML).toBe("<h2>About</h2><p><em>bold text</em></p>");
+
+    });
+
+});
